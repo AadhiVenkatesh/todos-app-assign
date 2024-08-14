22ac214e1bb9d2e74a2dfdd73a1f3dba5fc7aac7
AadhiVenkatesh <129151964+AadhiVenkatesh@users.noreply.github.com>
2024-08-14 20:40:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ const authenticateToken = (req, res, next) => {
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err)
       return res
-        .status(500)
+        .status(401)
         .json({ auth: false, message: "Failed to authenticate token." });
     req.user = user;
     next();
